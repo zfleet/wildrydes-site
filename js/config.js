c69eeb98da8d18848028dd9b92f2fdb9742c23c2
Teddy Poingt <zfleet@gmail.com>
2021-10-19 21:31:10 -0400

    add cognito conf

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ca-central-1_mHLwE9o5W', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '49acidkhcrtgj3kuij749k60qm', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ca-central-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
